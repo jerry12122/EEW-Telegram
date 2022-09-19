37af5fda18658215b0ba97af5ca6481c6e6f0078
jerry12122 <a0975844268@gmail.com>
2022-09-19 21:58:37 +0800

    config與新增/移除方式更新

--- README.md
+++ README.md
@@ -1,27 +1,33 @@
 ###### tags: `README`
-# 地牛Wake up Telegram通知連動
-配合地牛Wake Up! Windows版 https://eew.earthquake.tw   
 
-說明
----
-由於地牛wake up僅會顯示桌面通知，可以藉由此連動程式來發送telegram訊息通知
+# 地牛 Wake up Telegram 通知連動
 
-需求
----
-* Telegram Bot Token
-* Telegram User ID
-* Python環境
+配合地牛 Wake Up! Windows 版 https://eew.earthquake.tw
 
-使用說明
----
+## 說明
+
+由於地牛 wake up 僅會顯示桌面通知，可以藉由此連動程式來發送 telegram 訊息通知
+
+## 需求
+
+- Telegram Bot Token
+- Telegram User ID
+- Python 環境
+
+## 使用說明
+
 1. 安裝依賴套件
+
 ```
 pip install -r requirements.txt
 ```
-2. 將設定檔`config.json.sample`更改為`config.json`並新增bot_token  
-3. 使用指令來新增/移除uid
+
+2. 將設定檔`config.json.sample`更改為`config.json`並新增 bot_token
+3. 使用指令來新增/移除 uid
+
 ```
-python notifications.py [uid]
+python notifications.py [add/remove] [uid]
 ```
-4. 使用系統管理員打開地牛Wake up程式，打開設定>其他  
+
+4. 使用系統管理員打開地牛 Wake up 程式，打開設定>其他
 5. 啟用連動設定並選擇`start.bat`的路徑，儲存
--- notifications.py
+++ notifications.py
@@ -10,25 +10,24 @@ def sendMessage(token , uid, message):
 
 with open('config.json','r') as outfile:
     config = json.load(outfile)
-    token = config[0]["token"]
-    users = config[1:]
-    argc = len(sys.argv)
-    if argc > 2:
-        message = "地震震度{} \n即將在{}秒後抵達".format(sys.argv[1],sys.argv[2])
-        for i in users:
-            sendMessage(token,i["uid"],message)
-    else:
+    token = config["token"]
+    users = config["users"][0:]
+    if sys.argv[1] == "add" or sys.argv[1] == "remove":
         with open('config.json','w') as f:
-            add = 1
-            for i in users:
-                if i["uid"] == sys.argv[1]:
-                    add = 0
-                    users.remove(i)
-                    print("uid {} removed".format(i["uid"]))
-            if add:
-                user = dict()
-                user["uid"] = sys.argv[1]
-                users.append(user)
-                print("uid {} added".format(sys.argv[1]))
-            config = [config[0]] + users
-            f.write(json.dumps(config, ensure_ascii=False))
+            if sys.argv[1] == "add":
+                for user in users:
+                    if user == sys.argv[2]:
+                        users.remove(user)
+                        print("uid {} removed".format(user))
+            else:
+                users.append(sys.argv[2])
+                print("uid {} added".format(sys.argv[2]))
+                
+            data = dict()
+            data["token"] = token
+            data["users"] = users
+            f.write(json.dumps(data, ensure_ascii=False))
+    else:
+        message = "地震震度{} \n即將在{}秒後抵達".format(sys.argv[1],sys.argv[2])
+        for user in users:
+            sendMessage(token,user,message)
