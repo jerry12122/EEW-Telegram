99b23c02714e59af18076eeda46b6890158cef56
jerry12122 <a0975844268@gmail.com>
2022-09-19 23:05:38 +0800

    bug fix

--- notifications.py
+++ notifications.py
@@ -12,20 +12,24 @@ with open('config.json','r') as outfile:
     config = json.load(outfile)
     token = config["token"]
     users = config["users"][0:]
+    print(token)
+    print(users)
+    print(sys.argv)
     if sys.argv[1] == "add" or sys.argv[1] == "remove":
         with open('config.json','w') as f:
             if sys.argv[1] == "add":
                 for user in users:
                     if user == sys.argv[2]:
-                        users.remove(user)
+                        users.append(user)
                         print("uid {} removed".format(user))
             else:
-                users.append(sys.argv[2])
+                users.removed(sys.argv[2])
                 print("uid {} added".format(sys.argv[2]))
                 
             data = dict()
             data["token"] = token
             data["users"] = users
+            print(data)
             f.write(json.dumps(data, ensure_ascii=False))
     else:
         message = "地震震度{} \n即將在{}秒後抵達".format(sys.argv[1],sys.argv[2])
